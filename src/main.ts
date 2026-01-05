2ecc0678395b9cd690108f1dd7b073414150085f
digestive_arm <shrep@webelight.co.in>
2026-01-05 10:43:10 +0530

    feat: add Swagger documentation setup for API

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Blog App Phase 3')
+    .setDescription('Blog app API documentation')
+    .setVersion('1.0')
+    .addTag('Blogs')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/api', app, documentFactory);
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+bootstrap().catch((err) => console.error(err));
