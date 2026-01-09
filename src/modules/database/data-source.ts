50f91d32a0e6b620568ccb98d1a4ccace250e11b
digestive-arm <shreyp@webelight.co.in>
2026-01-09 17:43:09 +0530

    feat: add TypeORM migration scripts and update database configuration (#10)
    Co-authored-by: digestive_arm <shrep@webelight.co.in>

--- src/modules/database/data-source.ts
+++ src/modules/database/data-source.ts
@@ -10,10 +10,9 @@ export default new DataSource({
   username: process.env['DATABASE_USER'],
   password: process.env['DATABASE_PASSWORD'],
   database: process.env['DATABASE_NAME'],
-  entities: ['src/modules/**/*.entity.ts', 'dist/modules/**/*.entity.js'],
-  migrations: ['dist/migrations/*.{js,ts}'],
-  subscribers: [],
+  synchronize: false,
+  entities: ['src/**/*.entity.ts', 'dist/**/*.entity.js'],
+  migrations: ['src/migrations/*.{ts}'],
   migrationsRun: false,
   migrationsTableName: 'migrations',
-  synchronize: true,
 });
