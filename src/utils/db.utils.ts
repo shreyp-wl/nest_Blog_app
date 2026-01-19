e7fd089aa5e56432ccb81ca91a138cd6a0c15cdd
digestive_arm <shrep@webelight.co.in>
2026-01-19 17:30:08 +0530

    chor: add utility to find existing record in db.

---
+++ src/utils/db.utils.ts
@@ -0,0 +1,8 @@
+import { FindOptionsWhere, ObjectLiteral, Repository } from 'typeorm';
+
+export async function findExistingEntity<T extends ObjectLiteral>(
+  repository: Repository<T>,
+  where: FindOptionsWhere<T> | FindOptionsWhere<T>[],
+): Promise<boolean> {
+  return repository.exists({ where });
+}
