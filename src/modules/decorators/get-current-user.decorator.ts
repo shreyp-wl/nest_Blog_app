b0ef39b728b7b5fd8b91259977d5d2cc130feb59
digestive_arm <shrep@webelight.co.in>
2026-01-20 13:23:07 +0530

    feat: implement CurrentUser decorator.

---
+++ src/modules/decorators/get-current-user.decorator.ts
@@ -0,0 +1,9 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { RequestWithUser } from 'src/common/interfaces/request-with-user.interface';
+
+export const CurrentUser = createParamDecorator(
+  (_: unknown, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest<RequestWithUser>();
+    return request.user;
+  },
+);
